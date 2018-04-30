b6037285db663ac3c870fe070a038f551c8e75da
razod <33201441+razod@users.noreply.github.com>
2018-04-29 22:10:06 -0400

    Update sites.js

--- sites.js
+++ sites.js
@@ -1,5 +1,6 @@
 var sites = {
-  "rileydatlord": "RileyDatLord/RileyDatLord.github.io"
+  "rileydatlord": "RileyDatLord/RileyDatLord.github.io",
+  "subdomain":"user/repo"
 }
 // Blocked Sub-Domains:
 // Already used subdomains.
