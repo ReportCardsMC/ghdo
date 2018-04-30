61603190c29967a9fa0336ed1e3adfc290c759d1
RileyDatLord <rileydatlord@gmail.com>
2018-04-29 17:25:43 -0700

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.sendFile(__dirname + '/views/index.ejs');
 });
 
 // listen for requests :)
