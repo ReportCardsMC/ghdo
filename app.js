81b4ae594ec001ee9433d9ef0ecd6ef1141c5a25
razod <33201441+razod@users.noreply.github.com>
2018-04-29 20:27:38 -0400

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.ejs');
+  response.sendFile(__dirname + '/views/index');
 });
 
 // listen for requests :)
