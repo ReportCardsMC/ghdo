5671b8cf15d3be6f8de45359b7cd61865dcce6e5
RileyDatLord <rileydatlord@gmail.com>
2018-04-29 20:39:58 -0700

    Update sites.js

--- sites.js
+++ sites.js
@@ -1,3 +1,17 @@
+exports.api = function(type, type2) {
+  var invalid2 = "Invalid Type2"
+  var invalid = "Invalid Type"
+  if (type == "sites") {
+    if (type2 == "size") {
+      return sites.size;
+    } else {
+      return invalid2;
+    }
+  } else {
+    return invalid;
+  }
+}
+
 var sites = {
   "rileydatlord": "RileyDatLord/RileyDatLord.github.io",
   "raz": "Razod/ravioliweb"
